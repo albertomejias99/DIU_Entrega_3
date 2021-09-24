afe0537cbaef454005cd79cf89cab83d4ac8290a
albertomejias99 <albertito.mejias@gmail.com>
2021-09-24 12:25:33 +0100

    first commit

---
+++ src/main/java/com/mycompany/diu_entrega_3/Main.java
@@ -0,0 +1,9 @@
+package com.mycompany.diu_entrega_3;
+
+public class Main {
+    public static void main(String[] args) {
+        JFrame jFrame = new JFrame();
+        jFrame.setVisible(true);
+        
+    }
+}
